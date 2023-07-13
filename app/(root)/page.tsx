b77d2a4934dc638aad98a6a580ea78185d81ebdb
zesettler <gromovgrin@gmail.com>
2023-07-13 21:24:13 +0300

    modal components global fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import {Modal} from "@/components/ui/modal";
+import {useStoreModal} from "@/hooks/use-store-modal";
+
+import {useEffect} from "react";
 
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
     return (
         <div className="p-4">
-            <Modal title="Test" description="Test description" isOpen={true}>
-                Children
-            </Modal>
+            Root Page
         </div>
     );
 }
