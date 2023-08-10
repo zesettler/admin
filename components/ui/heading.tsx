408ccb99a48419205991381697d6ea0b1b0dc1ff
zesettler <gromovgrin@gmail.com>
2023-08-10 22:29:36 +0300

    searching prisma problem

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,18 +1,19 @@
 interface HeadingProps {
-    title: sting;
+    title: string;
     description: string;
-};
-
-export const Heading: React.FC<HeadingProps> = ({
+  }
+  
+  export const Heading: React.FC<HeadingProps> = ({
     title,
     description
-}) => {
-    return (
-        <div>
-            <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-            <p className="text-sm text-muted-foreground">
-                {description}
-            </p>
-        </div>
-    )
-}
+  }) => {
+    return ( 
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
+        <p className="text-sm text-muted-foreground">
+          {description}
+        </p>
+      </div>
+    );
+  };
+   
